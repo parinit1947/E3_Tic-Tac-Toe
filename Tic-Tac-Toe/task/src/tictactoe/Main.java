3b1d1bab4d31e64db345f232d09348c69b42ab64
parinit1947 <parinit1947@gmail.com>
2020-07-13 22:39:06 +0530

    Stage 2/5: The user is the gamemaster

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,10 +1,32 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+
+        char[] chars = str.toCharArray();
+
+        int count = 0;
+
+        System.out.println("---------");
+
+        for (char c : chars) {
+            if (count == 0) {
+                System.out.print("| ");
+            }
+            System.out.print(c + " ");
+            count++;
+            if (count == 3) {
+                System.out.println("|");
+                count = 0;
+            }
+        }
+
+        System.out.println("---------");
     }
 }
